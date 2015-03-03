8fe6e8265a6a3daba7ca75f549b38ac6374debac
Milo Trujillo <milo.trujillo@zdtunited.com>
2015-03-02 18:39:43 -0800

    Set broadcast address for test pool

--- hurricane.rb
+++ hurricane.rb
@@ -4,15 +4,15 @@ require 'socket'
 require 'digest/sha2' # Defaults to size '256', supports any sized key
 
 broadcastport = 33333
-broadcastaddr = ['10.0.0.255', broadcastport]
+broadcastaddr = ['192.168.0.255', broadcastport]
 
 localhostname = Socket.gethostname() # Get current node name
 digest = Digest::SHA2.new << localhostname
-announce = "Announce " + localhostname + " => " + digest.to_s() + "\n"
+announce = "Announce " + localhostname + " => " + digest.to_s()
 
 broadcast = UDPSocket.new
 broadcast.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
 broadcast.send(announce, 0, broadcastaddr[0], broadcastaddr[1])
 broadcast.close
 
-print announce
+print announce + "\n"
