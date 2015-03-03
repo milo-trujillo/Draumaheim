62b20a7fac30fae74971f882dc116b988bd1b0be
Milo Trujillo <milo.trujillo@zdtunited.com>
2015-03-02 18:29:10 -0800

    Adding first UDP broadcast related tests

---
+++ hurricane.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+require 'digest/sha2' # Defaults to size '256', supports any sized key
+
+broadcastport = 33333
+broadcastaddr = ['10.0.0.255', broadcastport]
+
+localhostname = Socket.gethostname() # Get current node name
+digest = Digest::SHA2.new << localhostname
+announce = "Announce " + localhostname + " => " + digest.to_s() + "\n"
+
+broadcast = UDPSocket.new
+broadcast.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+broadcast.send(announce, 0, broadcastaddr[0], broadcastaddr[1])
+broadcast.close
+
+print announce
---
+++ listenBroadcast.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/env perl
+#
+# This code adapted from http://sourceforge.net/p/netcat/bugs/42/
+#
+# There's a known bug in GNU netcat in which it switches from broadcast to
+# unicast mode after receiving one datagram. This is not acceptable for our
+# testing, so we'll use a quick Perl script instead.
+#
+
+#
+# UDP Listen
+#
+use IO::Socket::INET;
+use warnings;
+use strict;
+
+my $port = 33333;
+
+my ($sock);
+my $data;
+
+$sock=IO::Socket::INET->new(Proto => 'udp',
+LocalPort => $port) or die "Can't bind: $@\n";
+
+print scalar localtime().": Awaiting data...\n";
+while($sock->recv($data, 1024)) 
+{
+	my ($port, $ipaddr) = sockaddr_in($sock->peername);
+	my ($peerhost)=gethostbyaddr($ipaddr, AF_INET);
+	my ($peerip) = inet_ntoa($ipaddr);
+
+	print scalar localtime().": Rcvd $data from $peerhost $peerip\n";
+}
+
+print "Done\n";
